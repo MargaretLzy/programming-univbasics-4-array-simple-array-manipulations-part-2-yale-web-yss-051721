47f3b1af68833898442044a0059905996f9bbd5e
dynamic-daemon-637b <zl3087@columbia.edu>
2021-05-31 01:21:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,6 +4,9 @@ end
 def using_insert(array)
     array.insert
 end
-def using_insert(array)
-    array.insert
+def using_uniq(array)
+    array.uniq
+end
+def using_flatten(array)
+  array.flatten
 end
